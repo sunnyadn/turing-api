bd35f0f12a48202d50488c5a3e0a997cf3ec1e71
Sunny Young <sunny@mogara.org>
2016-02-17 00:34:53 +0800

    init

---
+++ index.py
@@ -0,0 +1,41 @@
+import tornado.web
+import tornado.httpclient
+import tornado.options
+
+import urllib
+import json
+import os
+
+TURING_KEY = os.environ["TURING_KEY"]
+TURING_URL = "http://www.tuling123.com/openapi/api"
+
+class QqHandler(tornado.web.RequestHandler):
+    def post(self):
+        print self.request.body
+
+        sender_id = self.get_argument("sender_id")
+        content = self.get_argument("content")
+
+        request = tornado.httpclient.HTTPRequest(TURING_URL, "POST",
+            body = urllib.urlencode({
+                                        "key" : TURING_KEY,
+                                        "info" : content,
+                                        "userid" : sender_id
+                                    }))
+
+        client = tornado.httpclient.AsyncHTTPClient()
+        client.fetch(request, self.on_response)
+
+    def on_response(self, response):
+        body = json.loads(response.body)
+        print response.body
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application([
+        (r"/qq", QqHandler),
+        # (r"/console", ConsoleHandler),
+    ], debug=True)
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
